0f8dd6111824339bff397dc1612924b5b0393a13
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-09-20 10:42:52 +0000

    tests commit

---
+++ entrypoint.sh
@@ -0,0 +1,4 @@
+git config --global --add safe.directory /holberton/github/holbertonschool-higher_level_programming
+git config --global user.name "jesus"
+git config --global user.email "jesusmanuel.maciasmartinez@gmail.com"
+
