c379ead0134c7b31fdf2802799ee55a6c8292875
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-09-21 23:44:01 -0500

    python loops last digit fits output error secont atent

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -11,4 +11,4 @@ if (ld > 5):
 elif (ld == 0):
     print(f"Last digit of {number} is {ld} and is 0")
 elif (ld < 6):
-    print(f"Last digit of {number} is {ld} and is less than 6 and is not 0")
+    print(f"Last digit of {number} is {ld} and is less than 6 and not 0")
