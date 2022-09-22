a0828087ab6da3ea1de4fd678ee31c6ad90c9060
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-09-22 06:40:06 -0500

    python loops print comb 2 fit output

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-for n in range(0, 99):
+for n in range(0, 100):
     if (n < 10):
         print("0{}, ".format(n), end="")
-    elif (n != 98):
+    elif (n != 99):
         print("{}, ".format(n, hex(n)), end="")
     else:
-        print("98", end="")
+        print("99", end="")
