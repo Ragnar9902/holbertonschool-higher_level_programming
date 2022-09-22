1f1d0a798401a70614051c62c7aa31f302e08eca
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-09-22 08:20:15 -0500

    python function add and pow

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def pow(a, b):
+    if b > 0:
+        temp = a
+        for i in range(0, b - 1):
+            temp = temp * a
+    elif b < 0:
+        temp = 1 / a
+        for i in range(0, -1 * b - 1):
+            temp = temp / a
+    else:
+        temp = 0
+    return temp
+
+
+print(pow(2, -3))
