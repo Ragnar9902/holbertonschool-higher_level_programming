4426ec4415f8a6669e47a72740beab8451b850ce
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-09-22 06:53:47 -0500

    python loops print comb 3 fit output

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 10):
     for j in range(0, 10):
         if (i == 8 and j > i):
-            print("{}{}".format(i, j), end="")
+            print("{}{}".format(i, j))
         elif (i < j ):
             print("{}{}".format(i, j), end=", ")
