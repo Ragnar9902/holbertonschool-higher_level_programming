185d5e3e85bf8831cf9f1a542fb457554eea5a16
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-09-22 06:53:02 -0500

    python loops print comb 3

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for j in range(0, 10):
+        if (i == 8 and j > i):
+            print("{}{}".format(i, j), end="")
+        elif (i < j ):
+            print("{}{}".format(i, j), end=", ")
