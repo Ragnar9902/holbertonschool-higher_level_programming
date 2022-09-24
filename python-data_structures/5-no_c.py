60fd894ff21df45441c86a1ec9a45d28ffee96f3
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-09-24 10:32:00 -0500

    data structure 5-no_c.py fit compile error

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    count = 0
-    for i in my_string:
-        if i == 'c' or i == 'C':
-            my_string[count] = ''
-        count += 1
-    return my_string
+    my_new_string = my_string.translate({ ord(c): None for c in "cC" })
+    return my_new_string
