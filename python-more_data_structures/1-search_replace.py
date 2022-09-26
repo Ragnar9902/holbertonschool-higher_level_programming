9e960cf5c476097907e5e5b0e916b98f47321349
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-09-26 17:59:52 -0500

    more data structures search and replace pycodestyle

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+from operator import index
+
+
 def search_replace(my_list, search, replace):
     new_list = []
-    for l, i in enumerate(my_list):
-        if i == search:
+    for index, element in enumerate(my_list):
+        if element == search:
             new_list.append(replace)
         else:
-            new_list.append(my_list[l])
+            new_list.append(my_list[index])
     return new_list
- 
