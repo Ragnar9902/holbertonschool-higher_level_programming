eb40f93c1ff8a15b9f83bb962ed41c6914a4070a
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-09-26 17:44:42 -0500

    more data structures fit implementation

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    squart_M = matrix.copy()
-    for l, i in enumerate(squart_M):
-        for k, j in enumerate(i):
-            squart_M[l][k] = j*j
+    squart_M = [[x**2 for x in row] for row in matrix]
     return squart_M
