c667a8f0a1943df75449b68536a2f1e25c15cecd
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-09-26 17:55:48 -0500

    more data structures search and replace

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+def search_replace(my_list, search, replace):
+    new_list = []
+    for l, i in enumerate(my_list):
+        if i == search:
+            new_list.append(replace)
+        else:
+            new_list.append(my_list[l])
+    return new_list
+ 
