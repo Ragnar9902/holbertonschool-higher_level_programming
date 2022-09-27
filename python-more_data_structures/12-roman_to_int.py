19bb68e760ce3287af5e6651d2a827e53955a5be
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-09-27 07:29:55 -0500

    more data structures roman to int

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def roman_to_int(Roman_string):
+    r_numbers = {"L": 50, "V": 5, "X": 10, "C": 100, "D": 500, "I": 1}
+    R_number = 0
+    for iter, i in enumerate(Roman_string):
+        if iter > 0 and r_numbers[i] > r_numbers[Roman_string[iter - 1]]:
+            R_number += r_numbers[i] - 2 * r_numbers[Roman_string[iter - 1]]
+        else:
+            R_number += r_numbers[i]
+    return R_number
