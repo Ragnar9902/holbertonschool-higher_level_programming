4efabeb21850c78d87d6f512953b2931380a6a28
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-09-27 16:17:04 -0500

    exceptions sefe print division use finally

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,6 +3,8 @@ def safe_print_division(a, b):
     try:
         result = a / b
         print("{:d}".format(result))
-        return result
     except:
-        pass
+        return None
+    finally:
+        print("Inside result: {:d}".format(result))
+        return result
