3e414d544d39ade8768590020ed1d2961c6d3062
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-09-29 10:35:53 -0500

    exceptions list division

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    if len(my_list_1) != len(my_list_1):
+        print("out of rage")
+    new_list = []
+    for i in range(0, list_length):
+        try:
+            div = my_list_1[i] / my_list_2[i]
+            div_s = True
+        except ZeroDivisionError:
+            print("division by 0")
+        except TypeError:
+            print("wrong type")
+        except IndexError:
+            pass
+        finally:
+            if div_s:
+                new_list.append(div)
+            else:
+                new_list.append(0)
+    return new_list
