65b0ae579211d8d876f882e55b688b6facb8fecf
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-09-29 10:04:47 -0500

    exceptions delf print division

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,9 +2,9 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-        print("{:d}".format(result))
-    except:
-        return None
+        print("{}".format(result))
+    except (ZeroDivisionError, FloatingPointError):
+        result = None
     finally:
-        print("Inside result: {:d}".format(result))
-        return result
+        print("Inside result: {}".format(result))
+    return result
