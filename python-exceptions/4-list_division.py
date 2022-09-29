ebe4165513484ea57fedbec0637ce5dacd687881
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-09-29 10:45:43 -0500

    exceptions case out of range

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -6,11 +6,14 @@ def list_division(my_list_1, my_list_2, list_length):
             div = my_list_1[i] / my_list_2[i]
             div_s = True
         except ZeroDivisionError:
+            div = 0
             print("division by 0")
         except TypeError:
+            div = 0
             print("wrong type")
         except IndexError:
             print("out of range")
+            div = 0
         finally:
             if div_s:
                 new_list.append(div)
