a95b2e2cb87ebc7e2b76d52d13ba64de9beb0f4b
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-09-30 00:02:12 -0500

    python classes square 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Python classes
+    this module is aid to practice the basis
+    of python clases
+"""
+
+class Square():
+    """This is a geometric figure
+    of four sides
+    """
+    def __init__(self, size=0) -> None:
+        """Initialization of the class square"""
+        if type(size) is int:
+            if size > 0:
+                self.__size = size
+            else:
+                raise ValueError
+        else:
+            raise TypeError        
