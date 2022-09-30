f29b73fc5316a8463de70f55c30450a5b2f7a885
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-09-30 00:08:21 -0500

    python classes square 2 fit output

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -10,10 +10,8 @@ class Square():
     """
     def __init__(self, size=0) -> None:
         """Initialization of the class square"""
-        if isinstance(size, int):
-            if size > 0:
-                self.__size = size
-            else:
-                raise ValueError
-        else:
+        if not isinstance(size, int):
             raise TypeError        
+        elif size < 0:
+                raise ValueError
+        self.__size = size
