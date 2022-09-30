120274b07c64553f9f879908bf38b54319d53bdb
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-09-30 00:30:22 -0500

    python classes square 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -9,7 +9,7 @@ class Square():
     """This is a geometric figure
     of four sides
     """
-    def __init__(self, size=0) -> None:
+    def __init__(self, size=0):
         """Initialization of the class square"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
@@ -18,4 +18,22 @@ class Square():
         self.__size = size
     
     def area(self):
-        return (self.__size ^ 2)
+        """Return the area of the square"""
+        return ((self.__size) ** 2)
+
+
+my_square_1 = Square(3)
+print("Area: {}".format(my_square_1.area()))
+
+try:
+    print(my_square_1.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
+
+my_square_2 = Square(5)
+print("Area: {}".format(my_square_2.area()))
