27fc44de33760c65c570b1fc22f6ee5aaeb27cd2
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-09-30 00:13:05 -0500

    python classes square 2 fit output

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,6 +4,7 @@
     of python clases
 """
 
+
 class Square():
     """This is a geometric figure
     of four sides
@@ -11,7 +12,7 @@ class Square():
     def __init__(self, size=0) -> None:
         """Initialization of the class square"""
         if not isinstance(size, int):
-            raise TypeError("size must be an integer")        
+            raise TypeError("size must be an integer")
         elif size < 0:
-                raise ValueError("size must be >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = size
