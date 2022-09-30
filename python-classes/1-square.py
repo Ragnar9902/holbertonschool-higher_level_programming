8f5df4ce087a1f8a988a6859e6e9541d8fa764d6
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-09-29 23:38:17 -0500

    python classes square 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class Square():
+    """This is a geometric figure
+    of four sides
+    """
+    def __init__(self, size) -> None:
+        """Initialization of the class square"""
+        self.__size = size
