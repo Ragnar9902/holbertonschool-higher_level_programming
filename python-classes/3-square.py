d9db8ab0c974e67c2462ae77eac9544ece131195
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-09-30 00:38:02 -0500

    python classes square 3 fit output

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -9,7 +9,6 @@ class Square():
     """This is a geometric figure
     of four sides
     """
-    __size = 0
     def __init__(self, size=0):
         """Initialization of the class square"""
         if not isinstance(size, int):
@@ -21,20 +20,3 @@ class Square():
     def area(self):
         """Return the area of the square"""
         return self.__size ** 2
-
-
-my_square_1 = Square(3)
-print("Area: {}".format(my_square_1.area()))
-
-try:
-    print(my_square_1.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)
-
-my_square_2 = Square(5)
-print("Area: {}".format(my_square_2.area()))
