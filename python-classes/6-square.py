f0d6e1f29b7afd37961b2c1d787db12576417cce
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-09-30 01:06:43 -0500

    python classes square 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""Python classes
+    this module is aid to practice the basis
+    of python clases
+"""
+
+
 class Square:
     """Represents a square.
     Private instance attribute: size:
