71bf655426dabae48f81437cbf11329d8ded59be
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-09-30 00:55:08 -0500

    python classes square 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Python classes
+    this module is aid to practice the basis
+    of python clases
+"""
+
+
+class Square():
+    """This is a geometric figure
+    of four sides
+    """
+    def __init__(self, size=0):
+        """Initialization of the class square"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the area of the square"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+            return
+        for i in range(0, self.__size):
+            for j in range(0, self.__size):
+                print("#", end="")
+            print("")
