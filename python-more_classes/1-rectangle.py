1226cb6d56b5488aba184422ac6fb774f08e72c3
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-10-04 17:10:25 -0500

    python more classes 1 rectangle fit output to task

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -16,9 +16,6 @@ class Rectangle:
     """
     def __init__(self, width=0, height=0) -> None:
 
-        if not isinstance(width, int) or not isinstance(height, int):
-            raise TypeError("width must be an integer")
-
         self.__height = height
         self.__width = width
 
@@ -28,6 +25,10 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -36,4 +37,8 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
         self.__height = value
