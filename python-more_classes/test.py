4b1e38974c303335ce46bdfb8aacc0221dc3ecaf
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-10-05 16:25:23 -0500

    python more classes 8-rectangle

--- python-more_classes/test.py
+++ python-more_classes/test.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-Rectangle = __import__('7-rectangle').Rectangle
-
-# create new instance based on representation
+Rectangle = __import__('8-rectangle').Rectangle
 
 my_rectangle_1 = Rectangle(8, 4)
-print(my_rectangle_1)
-my_rectangle_1.print_symbol = "H"
-print(my_rectangle_1)
+my_rectangle_2 = Rectangle(2, 3)
 
-my_rectangle_2 = Rectangle(2, 1)
-print(my_rectangle_2)
+if my_rectangle_1 is Rectangle.bigger_or_equal(my_rectangle_1, my_rectangle_2):
+    print("my_rectangle_1 is bigger or equal to my_rectangle_2")
+else:
+    print("my_rectangle_2 is bigger than my_rectangle_1")
+
+
+my_rectangle_2.width = 10
+my_rectangle_2.height = 5
+if my_rectangle_1 is Rectangle.bigger_or_equal(my_rectangle_1, my_rectangle_2):
+    print("my_rectangle_1 is bigger or equal to my_rectangle_2")
+else:
+    print("my_rectangle_2 is bigger than my_rectangle_1")
