da53b9dbfbe81fc4ff44af17bf2a4e48dccf1a9e
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-10-04 21:31:54 -0500

    python more classes 1 rectangle

---
+++ python-more_classes/test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+Rectangle = __import__('2-rectangle').Rectangle
+
+try:
+    my_rectangle = Rectangle(2, -3)
+    print(my_rectangle)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    my_rectangle = Rectangle(-2, 3)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
