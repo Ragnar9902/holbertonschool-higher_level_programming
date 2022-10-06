6cba54c27f7e7d2fc2b773ec13b14b14983e0c54
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-10-06 17:03:49 -0500

    python test driven developent 4-print square

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""test driven developent
+"""
+
+
+def print_square(size):
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(0, size):
+        for j in range(0, size):
+            print("#", end="")
+        print("")
