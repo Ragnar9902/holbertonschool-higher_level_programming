b6deaf1f7e380d4b3a7cdd3743f0559c605e9d0a
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-10-06 15:33:18 -0500

    python test driven developent 2-matrix divided

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Tests driven developent
+"""
+
+
+def matrix_divided(matrix, div):
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
+    for row in matrix:
+        for i in row:
+            if type(i) is not float and type(i) is not int:
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    matrix = [[round(x / div) for x in row] for row in matrix]
