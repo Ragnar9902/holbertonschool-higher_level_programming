6455287628f13b42729a67ec257c1fefb446fb70
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-10-06 13:36:33 -0500

    python test driven developent 0-add integer fit output

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,9 +6,9 @@
 def add_integer(a, b):
     """add two number if type integer or float
     """
-    if not isinstance(a, int) or not isinstance(b, int):
+    if not isinstance(a, int) or not isinstance(a, float):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int):    
+    if not isinstance(b, int) or not isinstance(b, float):    
         raise TypeError("b must be an integer")
     if isinstance(a, float) or isinstance(b, float):
         return int(a) + int(b)
