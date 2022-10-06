7d3de0eb72e8303fa493a8c971bf4b4f0104ead4
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-10-06 16:03:38 -0500

    python test driven developent 2-matrix divided fit output 6

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -9,12 +9,13 @@ def matrix_divided(matrix, div):
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
     rowL = len(matrix[0])
-    for iter, row in matrix:
-        if iter > 0 and len(row) != rowL:
-            raise TypeError("Each row of the matrix must have the same size")
+    for row in matrix:
         for i in row:
             if type(i) is not float and type(i) is not int:
                 raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
+    for row in matrix:
+        if iter > 0 and len(row) != rowL:
+            raise TypeError("Each row of the matrix must have the same size")
+
     matrix = [[round(x / div, 2) for x in row] for row in matrix]
     return matrix
