443b2cff7037ae7a3172d31cac588346dff3f01c
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-10-07 11:19:55 -0500

    python test driven developent max integer

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -4,6 +4,9 @@
 
 
 def text_indentation(text):
+    """
+    add format to a string to print
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     for delim in ".:?":
@@ -11,4 +14,3 @@ def text_indentation(text):
             [line.strip(" ") for line in text.split(delim)])
 
     print("{}".format(text), end="")
-
