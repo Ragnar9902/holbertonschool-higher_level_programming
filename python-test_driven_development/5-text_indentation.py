8f50bad646136facab9567ff6f64c6e9493fbb4e
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-10-07 03:00:14 -0500

    python test driven developent 5-text indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""test driven developent
+"""
+
+
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for delim in ".:?":
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)])
+
+    print("{}".format(text), end="")
+
