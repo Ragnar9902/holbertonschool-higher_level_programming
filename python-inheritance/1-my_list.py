74ca7aac761cfb4553bff37081249d0b7423795c
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-10-13 15:39:24 -0500

    python inheritance my list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,8 +5,10 @@
 
 class My_list(list):
     """extend the list class."""
-    
+
     def print_sorted(self):
+        """Prints the list, in ascending sort."""
+
         new_list = self[:]
         new_list.sort()
         print("{}".format(new_list))
