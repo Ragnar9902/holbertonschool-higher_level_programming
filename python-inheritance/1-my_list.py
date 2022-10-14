3644ed2410905cf2464cb6b0d3b28add642cd812
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-10-13 23:19:05 -0500

    python inheritance my list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,7 +5,7 @@ Creates a class inheriting from the list class.
 
 
 class MyList(list):
-    """yyList inherits from list."""
+    """Class MyList inherits from list."""
 
     def print_sorted(self):
         """Prints the list, in ascending sort."""
