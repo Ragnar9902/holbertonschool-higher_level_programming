20f2f3a800ad209a83226943012b8aa454ea997a
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-10-14 07:41:36 -0500

    python inheritance square 1

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -10,7 +10,6 @@ class Square(Rectangle):
     """class that inherits and extends the properties of Rectangle"""
     def __init__(self, size):
         self.integer_validator("size", size)
-        super().__init__()
         self.__size = size
 
     def area(self):
