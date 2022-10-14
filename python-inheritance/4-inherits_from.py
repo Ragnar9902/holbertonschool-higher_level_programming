cc990cb1fe2c8c3f6c3f45406e95d962f7dd30bb
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-10-14 00:26:34 -0500

    python inheritance inherits from

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module 4-inherits_from.
+Finds if the object is an instance of a class that inherited
+(directly or indirectly) from the specified class."""
+
+
+def inherits_from(obj, a_class):
+    """Determines if obj is an instance of a class that
+    inherited from a_class.
+    """
+
+    return isinstance(obj, a_class) and type(obj) != a_class
