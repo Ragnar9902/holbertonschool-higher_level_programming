05b10550d3f1e0f9726474c635d9804c06fc6407
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-10-14 00:15:05 -0500

    python inheritance is kind of class

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module 3-is_kind_of_class.
+"""
+
+
+def is_same_class(obj, a_class):
+    """determinated whether an object is istance of a class or
+    a class that inherite from that class
+    """
+
+    return isinstance(obj, a_class)
