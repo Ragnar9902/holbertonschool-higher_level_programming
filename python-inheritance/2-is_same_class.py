690bc2815083ca67f2dc6a5be6682f91814d6e6f
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-10-14 00:08:59 -0500

    python inheritance is same class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module 2-is_same_class.
+"""
+
+def is_same_class(obj, a_class):
+    """determinated whether an object is istance of a class"""
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
