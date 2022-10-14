6d7dbaed227a10372765cad79644cc574e60d999
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-10-13 23:07:33 -0500

    python inheritance my list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""Python inheritance
-proyect
+"""Module 1-my_list.
+Creates a class inheriting from the list class.
 """
 
 
-class My_list(list):
-    """Extend the list class."""
+class MyList(list):
+    """Class MyList inherits from list."""
 
     def print_sorted(self):
         """Prints the list, in ascending sort."""
+
         new_list = self[:]
         new_list.sort()
         print("{}".format(new_list))
