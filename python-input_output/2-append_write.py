9b3f5562db4365a69331402c5ba036b9a6e56f69
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-10-14 21:42:02 -0500

    python input output append to file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+"""Module 2-apped_write.
+Writes in the end text file.
+"""
+
+
+def write_file(filename="", text=""):
+    """add text in the end of the file filename.
+    """
+
+    with open(filename, 'a') as f:
+        return f.write(text)
