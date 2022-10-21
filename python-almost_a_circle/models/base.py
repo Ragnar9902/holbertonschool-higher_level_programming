a7fe7ea3582638e593f207334fd218a72f357e3f
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-10-20 23:34:15 -0500

    python almost a circle class base

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Modulo almost a circle
+python oriented programming
+"""
+
+
+class Base:
+    __nb_objects = 0
+    
+    def __init__(self, id=None):
+        if id == None:
+            self.__nb_objects += 1
+            self.id = self.__nb_objects
+        else:
+            self.id = id
