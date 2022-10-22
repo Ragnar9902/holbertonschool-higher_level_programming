c6ef1c01d5aa78482ad5e92acbcfc85dce56ed0b
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-10-21 21:24:09 -0500

    python almost a circle class square

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -104,11 +104,14 @@ class Rectangle(Base):
     def display(self):
         """display the rectangle
         """
-        for i in range(self.height + self.y):
-            if i < self.y:
-                print("")
-            if i >= self.y:
-                print(" " * self.x + "#" * self.width)
+        for y in range(0, self.__y):
+            print()
+        for i in range(0, self.__height):
+            for x in range(0, self.__x):
+                print(" ", end="")
+            for j in range(0, self.__width):
+                print("#", end="")
+            print()
 
     def __str__(self):
         """Returns a string representation of a Rectangle instance."""
