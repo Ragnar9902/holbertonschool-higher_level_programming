9ab3b1a9500516527022459f4b34d2d066e2fcd9
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-11-18 22:41:25 -0500

    model state

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,11 +2,13 @@
 """
 Write a script that lists all State objects from the database hbtn_0e_6_usa
 """
+
+
 import sys
 from model_state import Base, State
-
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -3,6 +3,8 @@
 Write a script that prints the first
 State object from the database hbtn_0e_6_usa
 """
+
+
 import sys
 from model_state import Base, State
 from sqlalchemy import (create_engine)
