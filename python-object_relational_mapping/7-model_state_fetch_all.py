388d211214b87cd36206cf0edece3ca52dcf699d
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-11-18 22:45:15 -0500

    model state

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Write a script that lists all State objects from the database hbtn_0e_6_usa
+"""Python classes
+    this module is aid to practice the basis
+    of python clases
 """
 
 
