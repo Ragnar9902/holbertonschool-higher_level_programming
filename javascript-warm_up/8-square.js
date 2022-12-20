222972d396e21f7ae84b77632619ed92bf708fe3
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-12-19 21:03:43 -0500

    add funcion

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@ let process = require('process')
 if (parseInt(process.argv[2])){    
     let n_times = parseInt(process.argv[2])
     let line = 'X'.repeat(n_times);
-    for (let i = 0; i <= n_times; i++){
+    for (let i = 1; i <= n_times; i++){
         console.log(line);
     }
 }
